c1a1d8e50f6768bd022ed42537348ce04446d352
Herman Berdnikov <applejay1802@gmail.com>
2025-05-28 18:36:56 -0400

    Add tags parsing to tests

--- include/util/lua.h
+++ include/util/lua.h
@@ -7,6 +7,8 @@
 #include <lua.h>
 #include <lualib.h>
 
+int lua_table_is_array(lua_State *L, int index, lua_Integer *max_out);
+
 json_object *lua_to_json(lua_State *L, int index);
 
 void json_to_lua(lua_State *L, struct json_object *obj);
--- src/modules/taf/taf.c
+++ src/modules/taf/taf.c
@@ -1,5 +1,6 @@
 #include "modules/taf/taf.h"
 
+#include "cmd_parser.h"
 #include "taf_tui.h"
 #include "test_case.h"
 #include "util/lua.h"
@@ -23,15 +24,80 @@ static int l_module_taf_register_test(lua_State *L) {
     int s = selfshift(L);
 
     const char *name = luaL_checkstring(L, s);
-    luaL_checktype(L, s + 1, LUA_TFUNCTION);
+    int body_index;
 
-    lua_pushvalue(L, s + 1);                  /* duplicate fn -> top */
+    cmd_test_options *opts = cmd_parser_get_test_options();
+    bool register_test = false;
+    if (opts->tags_amount == 0) {
+        register_test = true;
+    }
+
+    test_tags_t tags = {.tags = NULL, .amount = 0};
+
+    switch (lua_type(L, s + 1)) {
+    case LUA_TFUNCTION: {
+        // Second arg is test body, test has no tags, skipping third arg
+        body_index = 1;
+        break;
+    }
+    case LUA_TTABLE: {
+        // Got tags, check them & test body
+        lua_Integer max;
+        int is_array = lua_table_is_array(L, s + 1, &max);
+        if (!is_array) {
+            luaL_error(L, "Tags are supposed to be an array of strings, e.g.: "
+                          "{\"tag1\", \"tag2\"}");
+            return 0;
+        }
+
+        tags.amount = max;
+        tags.tags = malloc(sizeof(*tags.tags) * tags.amount);
+
+        for (lua_Integer i = 1; i <= max; ++i) {
+            lua_rawgeti(L, s + 1, i); // push tags[i]
+
+            if (!lua_isstring(L, -1)) {
+                return luaL_error(L, "Tag #%lld is not a string (got %s)",
+                                  (long long)i, luaL_typename(L, -1));
+            }
+
+            const char *tag = lua_tostring(L, -1);
+            tags.tags[i - 1] = strdup(tag);
+
+            for (size_t i = 0; i < opts->tags_amount; i++) {
+                if (!strcmp(tag, opts->tags[i])) {
+                    register_test = true;
+                }
+            }
+
+            lua_pop(L, 1); // pop tags[i]
+        }
+
+        body_index = 2;
+        luaL_checktype(L, s + 2, LUA_TFUNCTION);
+        break;
+    }
+    default: {
+        luaL_error(
+            L,
+            "Expected test body or array of tags for second argument, got %s.",
+            lua_typename(L, s + 1));
+        return 0;
+    }
+    }
+
+    if (!register_test) {
+        // Skipping this test, does not contain required tags
+        return 0;
+    }
+
+    lua_pushvalue(L, s + body_index);         /* duplicate fn -> top */
     int ref = luaL_ref(L, LUA_REGISTRYINDEX); /* pop & ref */
 
-    test_case_t test_case = {.name = name, .ref = ref};
+    test_case_t test_case = {.name = name, .tags = tags, .ref = ref};
     test_case_enqueue(&test_case);
 
-    return 0; /* no Lua returns */
+    return 0;
 }
 
 /* taf:millis() -> number */
