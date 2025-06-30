fab6c15eab6cb4be37a6c30d6daf8d0404d8e901
Herman Berdnikov <applejay1802@gmail.com>
2025-06-30 17:33:41 -0400

    Prettier multitarget-example

--- examples/multitarget-example/tests/common/tests.lua
+++ examples/multitarget-example/tests/common/tests.lua
@@ -7,17 +7,16 @@ local variables = require("variables." .. target)
 taf.test("Testing multitarget project", function()
 	local baudrate = variables.serial.baudrate
 	print("Serial baudrate for target '" .. taf.get_current_target() .. "' is " .. baudrate)
-	if target == "target1" then
-		assert(baudrate == 115200)
-	elseif target == "target2" then
-		assert(baudrate == 1500000)
-	end
-
 	local result = custom_lib.keyword2("this is ", "test ")
 	print(result)
 	if target == "target1" then
+		assert(baudrate == 115200)
 		assert(result == "this is test ")
 	elseif target == "target2" then
+		assert(baudrate == 1500000)
 		assert(result == "this is test hello")
+	else
+		-- Should never be reached
+		taf.log_critical("Unknown target '" .. target .. "'")
 	end
 end)
