d82449dc5469bd52b1f4ed8652f7fd70e8c90e24
Herman Berdnikov <applejay1802@gmail.com>
2025-07-14 00:27:23 -0400

    Update

--- selftest/tests/bootstrap/module-webdriver.lua
+++ selftest/tests/bootstrap/module-webdriver.lua
@@ -6,7 +6,7 @@ taf.test("Test minimal webdriver possibilities", { "module-webdriver" }, functio
 	taf.defer(function()
 		proc_handle:kill()
 	end)
-	taf.sleep(100) -- wait for the webdriver to start just to make sure
+	taf.sleep(1000) -- wait for the webdriver to start just to make sure
 	local session = wd.session_start({ port = 9515, headless = "chromedriver" })
 	taf.defer(function()
 		wd.session_end(session)
